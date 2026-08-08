1122d246804665601cd57585801487b361492992
Aburul <96700335+Aburul@users.noreply.github.com>
2026-08-08 16:09:43 +0800

    Update reader.js

--- js/reader.js
+++ js/reader.js
@@ -55,24 +55,19 @@ async function initReader() {
 
         if (DropboxEngine.isConnected()) {
 
-            const blob = await DropboxEngine.getBookBlob(book.path);
+    const blob = await DropboxEngine.getBookBlob(book.path);
 
-            console.log("EPUB Blob:", blob);
-            console.log("Type:", blob.type);
-            console.log("Size:", blob.size);
+    console.log("EPUB Blob:", blob);
+    console.log("Type:", blob.type);
+    console.log("Size:", blob.size);
 
-            const arrayBuffer = await blob.arrayBuffer();
+    const arrayBuffer = await blob.arrayBuffer();
 
-            console.log(
-                "ArrayBuffer:",
-                arrayBuffer.byteLength
-            );
+    console.log("ArrayBuffer:", arrayBuffer.byteLength);
 
-            epubBook = ePub(arrayBuffer, {
-                openAs: "epub"
-            });
+    epubBook = ePub(arrayBuffer);
 
-        } else {
+} else {
 
             epubBook = ePub(book.path);
 
